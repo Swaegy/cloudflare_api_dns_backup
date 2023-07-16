e60326a9cc691a92baec06a3cf58f0a1811fd628
ARC-XX <76841018+ARC-XX@users.noreply.github.com>
2023-07-16 12:59:49 +0200

    Update cloudflare_api_dns_backup.sh
    Change Order of Variables and group them

--- cloudflare_api_dns_backup.sh
+++ cloudflare_api_dns_backup.sh
@@ -4,12 +4,14 @@
 APIKEY=""
 # DNS Zone ID, Cloudflare DNS Zone Overview
 ZONE=""
-# URL to Cloudflare API
-HOST="api.cloudflare.com"
 # Path to where the Export of your DNS Zone should be saved
 PATHCONFIG=""
+
+# URL to Cloudflare API
+HOST="api.cloudflare.com"
 # Variable to Add to Name for unique naming for daily export
 DATE=$(date +%Y%m%d) 
+
 
 # make the api request for config as xml and alias as json
 curl -X GET "https://${HOST}/client/v4/zones/${ZONE}/dns_records/export" -H "Authorization: Bearer ${APIKEY}" -H "Content-Type: application/json" -o "${PATHCONFIG}"/cloudflare/cloudflare-dnszone-"${DATE}".txt
