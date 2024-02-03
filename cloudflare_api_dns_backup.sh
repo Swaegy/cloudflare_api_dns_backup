fa96bb1a0bbe76fc884d36e1a9aab167bd9fc101
ARC-XX <76841018+ARC-XX@users.noreply.github.com>
2024-02-03 08:10:32 +0100

    Update cloudflare_api_dns_backup.sh
    Add --create-dirs to curl to create output directory if needed

--- cloudflare_api_dns_backup.sh
+++ cloudflare_api_dns_backup.sh
@@ -14,7 +14,7 @@ DATE=$(date +%Y%m%d)
 
 
 # make the api request for config as xml and alias as json
-curl -X GET "https://${HOST}/client/v4/zones/${ZONE}/dns_records/export" -H "Authorization: Bearer ${APIKEY}" -H "Content-Type: application/json" -o "${PATHCONFIG}"/cloudflare/cloudflare-dnszone-"${DATE}".txt
+curl -X GET "https://${HOST}/client/v4/zones/${ZONE}/dns_records/export" -H "Authorization: Bearer ${APIKEY}" -H "Content-Type: application/json" --create-dirs -o "${PATHCONFIG}"/cloudflare/cloudflare-dnszone-"${DATE}".txt
 
 # check the backup destination for backups older then 14 days
 find "${PATHCONFIG}" -type f -name cloudflare\*.txt -mtime +14 -delete
